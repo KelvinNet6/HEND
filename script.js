f657ba84b1a826afb400ea0a4f7b505d613f88c0
Krlvin <kelvin.net6@gmail.com>
2025-05-20 21:03:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-  <script>
-        const slides = document.querySelectorAll('.slideshow img');
+const slides = document.querySelectorAll('.slideshow img');
         let currentSlide = 0;
 
         setInterval(() => {
             slides[currentSlide].classList.remove('active');
             currentSlide = (currentSlide + 1) % slides.length;
             slides[currentSlide].classList.add('active');
-        }, 3000);
-    </script>
+  }, 3000);
